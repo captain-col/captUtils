853c4e7a4610d0fdb6a957e5234bdf360f066a0b
Jorge Chaves <jchaves@sas.upenn.edu>
2018-08-30 10:37:21 -0400

    Data-only fits

--- scripts/fitXsec.C
+++ scripts/fitXsec.C
@@ -1,4 +1,14 @@
-void fitXsec() {
+bool inBeamXY(double x, double y){
+    double a = -0.1188;
+    double b = -54.9894;
+    double width = 54/2;
+    double dist=fabs(a*x-y+b)/sqrt(a*a+1); 
+    if(dist<width ) return true;
+    return false;    
+}
+
+
+void fitXsecChi2() {
 
     TChain *c_data = new TChain("tracks");
     TChain *c_mc = new TChain("tracks");
@@ -55,6 +65,7 @@ void fitXsec() {
     TH1F * h_scan_1 = new TH1F("h_scan_1","",maxScan,0,maxScan);
     TH1F * h_scan_2 = new TH1F("h_scan_2","",maxScan,0,maxScan);
     TH1F * h_scan_3 = new TH1F("h_scan_3","",maxScan,0,maxScan);
+    TString xtitle = "TMath::Exp(-0.01*(x))";
     
     for (int nScan = 0; nScan < maxScan; nScan++) {
 	TH1F* hs_mc_0 = new TH1F("hs_mc_0","",40,-500,500);
@@ -63,7 +74,6 @@ void fitXsec() {
 	TH1F* hs_mc_3 = new TH1F("hs_mc_3","",40,0,700);
 
 	float weight = TMath::Exp(-float(1./maxScan)*(nScan));
-	TString xtitle = "TMath::Exp(-0.01*(x))";
 	//std::cout<<"weight="<<weight<<std::endl;
 	//weight = 1.0;
 	
@@ -126,5 +136,66 @@ void fitXsec() {
     leg->SetFillColor( kWhite ) ; 
     leg->Draw(); 
 
+    
+}
+
+void fitXsecDataOnly( double Emin, double Emax) {
+    TChain *c_data = new TChain("tracks");
+    //c_data->Add("tracks_golden.root");
+    c_data->Add("~/work/captain/software/work-area/captSummary/cmt/nb_mtpc_spl_012*_000_flat_beam*.root");
+
+    std::vector<double> *track_length = 0;
+    std::vector<double> *first_hit_X = 0;
+    std::vector<double> *first_hit_Y = 0;
+    std::vector<double> *first_hit_Z = 0;
+    std::vector<double> *corrected_first_hit_Z = 0;
+    std::vector<double> *corrected_PDS_energy = 0;
+    
+    c_data->SetBranchAddress("track_length",&track_length);
+    c_data->SetBranchAddress("first_hit_X",&first_hit_X);
+    c_data->SetBranchAddress("first_hit_Y",&first_hit_Y);
+    c_data->SetBranchAddress("first_hit_Z",&first_hit_Z);
+    c_data->SetBranchAddress("corrected_first_hit_Z",&corrected_first_hit_Z);
+    c_data->SetBranchAddress("corrected_PDS_energy",&corrected_PDS_energy);
+    
+    TH1F* hs_data = new TH1F("hs_data","",60,-500,100);
+
+    Int_t nEntries = c_data->GetEntries();
+
+    for (int iev=0; iev<nEntries; iev++) {
+	c_data->GetEntry(iev);
+	for (int t=0; t<first_hit_X->size(); t++) {
+	    double x = first_hit_X->at(t);
+	    double y = first_hit_Y->at(t);
+	    double z = corrected_first_hit_Z->at(t);
+	    double e = corrected_PDS_energy->at(t);
+	    if (inBeamXY(x,y) && z > -195 && z < -145 && x < 0.0 && x > -400 && e > Emin && e < Emax) {
+		hs_data->Fill(x);
+	    }
+	}
+    }
+
+    hs_data->Draw("ep");
+    TCanvas *c = new TCanvas("c");
+    TF1 *func = new TF1("func","expo",-400,0);
+    hs_data->Fit("func","R");
+    hs_data->Draw("ep");
+    TString fname;
+    fname.Form("fits/fit_%.1f_%.1f.pdf",Emin,Emax); 
+    c->Print(fname);
+    
+    hs_data->Delete();
+    //c->Delete();
+}
+
+void fitXsec() {
+
+    fitXsecDataOnly(100.0,200.0);
+    fitXsecDataOnly(200.0,300.0);
+    fitXsecDataOnly(300.0,400.0);
+    fitXsecDataOnly(400.0,500.0);
+    fitXsecDataOnly(500.0,600.0);
+    fitXsecDataOnly(600.0,700.0);
+    fitXsecDataOnly(700.0,800.0);
     
 }
